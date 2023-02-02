90ec5e1ab4492ff8f811971fe9e0e0d15c9a675f
FranklinJamesJones89 <fjones89@icloud.com>
2023-02-02 13:00:52 -0500

    Modify Navbar

--- frontend/src/components/Navbar/Navbar.js
+++ frontend/src/components/Navbar/Navbar.js
@@ -7,7 +7,16 @@ import './Navbar.css';
 const Navbar = () => {
 	return (
 		<nav className='app__navbar'>
-			Navbar
+			<div className='app__navbar-logo'>
+				<h1>Bistro Milliard</h1>
+			</div>
+			<ul className='app__navbar-links'>
+				<li className='p__opensans'><a href='#home'>Home</a></li>
+				<li className='p__opensans'><a href='#about'>About</a></li>
+				<li className='p__opensans'><a href='#menu'>Menu</a></li>
+				<li className='p__opensans'><a href='#awards'>Awards</a></li>
+				<li className='p__opensans'><a href='#contact'>Contact</a></li>
+			</ul>
 		</nav>
 	)
 }
