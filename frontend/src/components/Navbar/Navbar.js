b0e7c951b889e217bbc3e0b011dae84e9686423a
FranklinJamesJones89 <fjones89@icloud.com>
2023-02-02 12:26:38 -0500

    Create Navbar component

---
+++ frontend/src/components/Navbar/Navbar.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import { GiHamburgerMenu } from 'react-icons/gi';
+import { MdOutlineRestaurantMenu } from 'react-icons/md';
+
+import './Navbar.css';
+
+const Navbar = () => {
+	return (
+		<nav className='app__navbar'>
+			Navbar
+		</nav>
+	)
+}
+
+
+export default Navbar;
